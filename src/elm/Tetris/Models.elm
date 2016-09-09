c3a7b3b8a2dd467b71e63ab451722f31bb0b1d1a
staab <shtaab@gmail.com>
2016-09-09 07:05:17 -0700

    Moving to less matrix-oriented data model

--- src/elm/Tetris/Models.elm
+++ src/elm/Tetris/Models.elm
@@ -1,7 +1,7 @@
 module Tetris.Models exposing (..)
 
 import Maybe
-import Matrix exposing (Location, Matrix, matrix)
+import Matrix exposing (Location, Matrix, matrix, flatten)
 
 type BlockType
   = EmptyBlock
@@ -18,23 +18,29 @@ type ShapeType
 type alias Block =
   { blockType : BlockType
   , letter : Maybe String
+  , location : Location
   }
 
 type alias Shape =
   { shapeType : ShapeType
+  , blocks : List Block
   }
 
 type alias Model =
-  { blocks : Matrix Block
+  { blocks : List Block
   , shape : Maybe Shape
   }
 
 initialModel : Model
 initialModel =
-  { blocks = matrix 20 15 (\location -> { blockType = EmptyBlock, letter = Nothing} )
+  { blocks = flatten ( matrix 20 15 (\location -> newBlock EmptyBlock Nothing location ) )
   , shape = Nothing
   }
 
-newShape : ShapeType -> Maybe Shape
-newShape shapeType =
-  Just { shapeType = shapeType }
+newBlock : BlockType -> Maybe String -> Location -> Block
+newBlock blockType letter location =
+  { blockType = EmptyBlock, letter = Nothing, location = location }
+
+newShape : ShapeType -> List Block -> Maybe Shape
+newShape shapeType blocks =
+  Just { shapeType = shapeType, blocks = blocks }
--- src/elm/Tetris/Update.elm
+++ src/elm/Tetris/Update.elm
@@ -2,8 +2,10 @@ module Tetris.Update exposing (update, tick)
 
 import Time
 import Random.Pcg exposing (Seed, step)
+import Matrix exposing (loc, mapWithLocation)
+import Utils exposing (fromJust)
 import Tetris.Messages exposing (Msg(..))
-import Tetris.Models exposing (Model, Block, BlockType(..), newShape)
+import Tetris.Models exposing (Model, Shape, Block, BlockType(..), newShape, newBlock)
 import Tetris.Utils exposing (randomShapeType)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -23,7 +25,19 @@ addShape : Seed -> Model -> Model
 addShape seed model =
   let
     ( shapeType, seed ) = randomShapeType seed
+    shape = newShape
+      shapeType
+      [ newBlock FullBlock ( Just "A" ) ( loc 1 0 )
+      , newBlock FullBlock ( Just "B" ) ( loc 2 0 )
+      ]
   in
     { model |
-      shape = newShape shapeType
+      shape = shape,
+      blocks = applyShape model.blocks ( fromJust shape )
     }
+
+applyShape : List Block -> Shape -> List Block
+applyShape blocks shape =
+  List.map (\block -> block) blocks
+
+
