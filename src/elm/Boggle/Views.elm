c664f40b4aeb1a6cc8964905688b6851a30577ce
staab <shtaab@gmail.com>
2016-12-22 17:58:31 -0800

    Some alignment

--- src/elm/Boggle/Views.elm
+++ src/elm/Boggle/Views.elm
@@ -1,5 +1,6 @@
 module Boggle.Views exposing (leftSidebar, rightSidebar)
 
+import String exposing (padLeft)
 import Html exposing (Html, div, span, text, strong, i, a, button)
 import Html.Attributes exposing (class, href, target, style)
 import Html.Events exposing (onClick)
@@ -11,27 +12,32 @@ leftSidebar : Model -> Html Msg
 leftSidebar model =
     div
         [ class "left-sidebar" ]
-        [ div [ class "current-score" ] [ text ("Score: " ++ (toString model.score)) ]
-        , div
+        [ div
             [ style [ ( "display", "inline-block" ) ] ]
             [ div
-                [ style
-                    [ ( "display", "flex" )
-                    , ( "flex-direction", "column" )
-                    , ( "align-items", "center" )
-                    , ( "justify-content", "space-around" )
-                    , ( "height", "100px" )
+                [ class "current-score" ]
+                [ text (model.score |> toString |> padLeft 3 '0') ]
+            , div
+                [ style [ ( "display", "inline-block" ) ] ]
+                [ div
+                    [ style
+                        [ ( "display", "flex" )
+                        , ( "flex-direction", "column" )
+                        , ( "align-items", "center" )
+                        , ( "justify-content", "space-around" )
+                        , ( "height", "100px" )
+                        ]
                     ]
-                ]
-                [ i
-                    [ onClick TogglePaused, class "fa fa-btn fa-2x fa-pause" ]
-                    []
-                , a
-                    [ href "https://cash.me/$jstaab"
-                    , target "_blank"
-                    , onClick TogglePaused
-                    ]
-                    [ i [ class "fa fa-btn fa-2x fa-heart" ] []
+                    [ i
+                        [ onClick TogglePaused, class "fa fa-btn fa-2x fa-pause" ]
+                        []
+                    , a
+                        [ href "https://cash.me/$jstaab"
+                        , target "_blank"
+                        , onClick TogglePaused
+                        ]
+                        [ i [ class "fa fa-btn fa-2x fa-heart" ] []
+                        ]
                     ]
                 ]
             ]
